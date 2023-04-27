875fed74093efea924f3064266598e8d2fb34006
eMobilis <nathnaelsherry@outlook.com>
2023-04-27 10:30:41 +0300

    firebase RT App

--- app/src/main/java/com/example/firebase_rt_nate/Login_Activity.kt
+++ app/src/main/java/com/example/firebase_rt_nate/Login_Activity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login_Activity : AppCompatActivity() {
     lateinit var mail_lgn:EditText
     lateinit var pass_lgn:EditText
     lateinit var loginButton_lgn:Button
     lateinit var registerButton_lgn: Button
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,28 @@ class Login_Activity : AppCompatActivity() {
         loginButton_lgn = findViewById(R.id.btnLoginlgn)
         registerButton_lgn = findViewById(R.id.btnRegisterlgn)
 
+        auth = FirebaseAuth.getInstance()
+
         loginButton_lgn.setOnClickListener {
-            //-+
+            var email = mail_lgn.text.toString().trim()
+            var password = pass_lgn.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Cannot Submit an Empty Field", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+
+                        //navigate a user to a different page
+                        var gotomain = Intent(this, MainActivity::class.java)
+                        startActivity(gotomain)
+                        finish()
+                    }else{
+                        Toast.makeText(this, "Sorry failed to login", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
 
         registerButton_lgn.setOnClickListener {
@@ -34,3 +57,6 @@ class Login_Activity : AppCompatActivity() {
 
     }
 }
+
+
+//https://justpaste.it/8y1hy
--- app/src/main/java/com/example/firebase_rt_nate/RegisterActivity.kt
+++ app/src/main/java/com/example/firebase_rt_nate/RegisterActivity.kt
@@ -1,15 +1,21 @@
 package com.example.firebase_rt_nate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var name_reg:EditText
     lateinit var mail_reg:EditText
     lateinit var pass_reg:EditText
     lateinit var createAcc_btn:Button
+
+    //initialize firebase
+    lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +26,34 @@ class RegisterActivity : AppCompatActivity() {
         pass_reg = findViewById(R.id.edtPassReg)
         createAcc_btn = findViewById(R.id.btnCreateAcc)
 
+        auth = FirebaseAuth.getInstance()
+
 
         createAcc_btn.setOnClickListener {
+            var email = mail_reg.text.toString().trim()
+            var name = name_reg.text.toString().trim()
+            var password = pass_reg.text.toString().trim()
 
+            //validate user info
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Cannot Submit Empty Fields", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "User Created Successfully", Toast.LENGTH_SHORT).show()
+
+                        //navigate back to login
+                        var gotologin = Intent(this, Login_Activity::class.java)
+                        startActivity(gotologin)
+                    }else{
+                        Toast.makeText(this, "Failed To Create Account", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
 
     }
 }
-//git config user.email "nathnaelsherry@outlook.com"
+
+
+//https://justpaste.it/cikxl
