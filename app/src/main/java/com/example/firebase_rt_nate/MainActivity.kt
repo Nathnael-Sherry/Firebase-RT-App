908ab86816c5cad2441c37ada9ff8e859cf4f19c
eMobilis <nathnaelsherry@outlook.com>
2023-04-28 10:52:29 +0300

    rt database

--- app/src/main/java/com/example/firebase_rt_nate/MainActivity.kt
+++ app/src/main/java/com/example/firebase_rt_nate/MainActivity.kt
@@ -8,27 +8,80 @@ import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-    lateinit var data_entry:EditText
-    lateinit var save_button:Button
+    lateinit var carmake_edt:EditText
+    lateinit var carmodel_edt:EditText
+    lateinit var carprice_edt:EditText
+
+    lateinit var car_view:Button
+    lateinit var upload_car_photo:Button
+    lateinit var upload_car_data:Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        data_entry = findViewById(R.id.edtdata)
-        save_button = findViewById(R.id.btnSave)
+        carmake_edt = findViewById(R.id.edtcarmake)
+        carmodel_edt = findViewById(R.id.edt_carmodel)
+        carprice_edt = findViewById(R.id.edt_carprice)
+
+        car_view = findViewById(R.id.btn_view_cars)
+        upload_car_photo = findViewById(R.id.btn_carphoto)
+        upload_car_data = findViewById(R.id.btn_car_data)
+
 
         var database = FirebaseDatabase.getInstance()
-        var databaseRef = database.reference
+        var databaseRef = database.getReference("cars")
 
-        save_button.setOnClickListener {
-            var user_data = data_entry.text.toString().trim()
 
-            databaseRef.setValue(user_data)
 
-//            Toast.makeText(this, user_data, Toast.LENGTH_SHORT).show()
+
+
+
+        //viewing uloaded data
+        car_view.setOnClickListener {
+
         }
 
-    }
+
+
+        //uploading all data
+        upload_car_data.setOnClickListener {
+
+            var carmake = carmake_edt.text.toString().trim()
+            var carprice = carprice_edt.text.toString().trim()
+            var carmodel = carmodel_edt.text.toString().trim()
+
+            if (carmake.isEmpty() || carprice.isEmpty() || carmodel.isEmpty()){
+                Toast.makeText(this, "cannot submit empty fields", Toast.LENGTH_SHORT).show()
+            }else{
+
+                //try to upload data to firebase
+                var usercar = car(carmake, carmodel, carprice)
+
+                //creata a reference to the firebase cloud
+                var ref = FirebaseDatabase.getInstance().getReference().child("cars")
+
+                ref.setValue(usercar).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Car Data Uploaded Successfully", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "Failed to save car info", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            }
+
+        }
+
+
+
+        //uploading photo
+        upload_car_photo.setOnClickListener {
+
+        }
+   }
 }
+
+//https://justpaste.it/bczmq
